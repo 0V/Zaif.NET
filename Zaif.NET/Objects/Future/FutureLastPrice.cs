cb7eca38bc3e08bff1ebac0350427b05fa4df9f0
G2 <g2simm+0v@gmail.com>
2017-12-09 17:19:55 +0900

    Add Future API objects

---
+++ Zaif.NET/Objects/Future/FutureLastPrice.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaifNet
+{
+    public class FutureLastPrice
+    {
+        [JsonProperty("last_price")]
+        public double LastPrice { get; set; }
+
+        [JsonProperty("group_id")]
+        public int GroupId { get; set; }
+    }
+}
---
+++ Zaif.NET/Objects/Future/Groups.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ZaifNet
+{
+    public class Groups
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("currency_pair")]
+        public string CurrencyPair { get; set; }
+
+        [JsonProperty("start_timestamp")]
+        public string StartTimestamp { get; set; }
+
+        [JsonProperty("end_timestamp")]
+        public string EndTimestamp { get; set; }
+
+        [JsonProperty("use_swap")]
+        public bool UseSwap { get; set; }
+
+    }
+}

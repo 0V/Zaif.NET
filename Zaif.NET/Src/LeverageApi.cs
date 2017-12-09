48ac03bda59a1731370b79d2c171c07b2ad50683
G2 <g2simm+0v@gmail.com>
2017-12-09 18:52:48 +0900

    Change Future to FuturePublic

---
+++ Zaif.NET/Src/LeverageApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace ZaifNet
+{
+    public class LeverageApi
+    {
+    }
+}
